4984d1dddc5a89c308c8acbfea991651d28414d7
Jeroen Breuer <jbreuer@gmail.com>
2016-09-30 10:12:54 +0200

    The Sample project now uses the CopernicaConfigurableDatabase and CopernicaConfigurableCollection attributes.

--- Src/CopernicaNET.Sample/CopernicaHandler.cs
+++ Src/CopernicaNET.Sample/CopernicaHandler.cs
@@ -1,17 +1,10 @@
-﻿using System.Configuration;
-using Arlanet.CopernicaNET.Sample.Models;
+﻿using Arlanet.CopernicaNET.Sample.Models;
 using Arlanet.CopernicaNET.Interfaces.Handlers;
 
 namespace Arlanet.CopernicaNET.Sample
 {
     public class CopernicaHandler : Copernica<CopernicaHandler>, ICopernicaHandlerBase
     {
-        public CopernicaHandler()
-            : base(ConfigurationManager.AppSettings["CopernicaAccessToken"])
-        {
-
-        }
-
         public void RegisterDataItems()
         {
             RegisterDataItem(new Client());
--- Src/CopernicaNET.Sample/Models/Product.cs
+++ Src/CopernicaNET.Sample/Models/Product.cs
@@ -3,14 +3,14 @@ using Arlanet.CopernicaNET.Data;
 
 namespace Arlanet.CopernicaNET.Sample.Models
 {
-    [CopernicaDatabase(108)]
-    [CopernicaCollection(733)]
+    [CopernicaConfigurableDatabase(typeof(Product))]
+    [CopernicaConfigurableCollection(typeof(Product))]
     public class Product : CopernicaSubprofile
     {
-        [CopernicaField("Naam", Type = CopernicaFieldTypes.TextField, Length = 50)]
+        [CopernicaField("ProductName", Type = CopernicaFieldTypes.TextField, Length = 50)]
         public string Name { get; set; }
 
-        [CopernicaField("Prijs", Type = CopernicaFieldTypes.IntField, Length = 50)]
+        [CopernicaField("Price", Type = CopernicaFieldTypes.IntField, Length = 50)]
         public int Price { get; set; }
 
     }
